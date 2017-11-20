6849f3d5c21badf4c8d9955b6040569bfb558398
Jeff Stein <jstein@mitre.org>
2017-11-20 12:49:06 -0500

    Reformatted many things - hopefully broke nothing

--- save_selected_speaker.py
+++ save_selected_speaker.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-import os,sys
+import os, sys
 from workflow import Workflow3
 
-def main(wf):
 
-	if sys.argv[1] != '':
-		selected_speaker = str(sys.argv[1])
-		wf.store_data('speaker_name',selected_speaker)
+def main(wf):
+    if sys.argv[1] != '':
+        selected_speaker = str(sys.argv[1])
+        wf.store_data('speaker_name', selected_speaker)
 
 
 if __name__ == '__main__':
-    wf = Workflow3(libraries=['./lib'])    
+    wf = Workflow3(libraries=['./lib'])
     sys.exit(wf.run(main))
